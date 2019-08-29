40d653f6e47bdb19ddeb8ab837981a3542a89f00
Anas <com.anas.idms.appleid.prd.001784-08-5d674918-865b-4553-9bf0-c2326b981e34@AB-Macbook.local>
2019-08-29 15:36:01 -0700

    condition statments, added update, is_empty, out of range, is number

--- checklist.py
+++ checklist.py
@@ -27,30 +27,69 @@ def list_all_items():
 def mark_completed(index):
     checklist[index] = "√"+checklist[index]
 
+# user input
 def user_input(prompt):
-    # the input function will display a message in the terminal
-    # and wait for user input.
     user_input = input(prompt)
     return user_input
 
+def is_empty():
+    if not checklist:
+        user_input("List is empthy, Press Enter key to go back: ")
+        return True
+    return False
+
+# check if user input is a number
+def is_number(index):
+    while(index.isdigit() == False):
+        index = user_input("That's not a number, please an index number: ")
+    return index
+
+# Check if user input is within range
+def out_of_range(index):
+    while(int(index) >= len(checklist)):
+        msg = "Index out of range, please enter an index between 0 and "+str(len(checklist)-1)+": "
+        index = user_input(msg)
+        index = is_number(index)
+    return index
 
 def select(function_code):
-    # Create item
-
     if function_code.capitalize() == "C":
         #Create item in checklist here
         item = user_input("Add to list: ")
         create(item)
 
-
     elif function_code.capitalize() == "R":
         # Read item in checklist here
-        index = user_input("Index of item you want to read: ")
-        print(read(int(index)))
+        if(is_empty() == False):
+            index = user_input("Index of item you want to read: ")
+            index = is_number(index)
+            index = out_of_range(index)
+            print(read(int(index)))
+
+    elif function_code.capitalize() == "D":
+        # delete item in checklist here
+        if(is_empty() == False):
+            index = user_input("Index of item you want to delete: ")
+            index = is_number(index)
+            index = out_of_range(index)
+            destroy(int(index))
+            list_all_items()
 
     elif function_code.capitalize() == "P":
         # Print all items here
         list_all_items()
+
+    elif function_code.capitalize() == "U":
+        # Print all items here
+        if(is_empty() == False):
+            index = user_input("Index of item you want to update: ")
+            index = is_number(index)
+            index = out_of_range(index)
+            item = user_input("Enter new item: ")
+            update(int(index), item)
+
+        list_all_items()
+
 
     elif function_code.capitalize() == "Q":
         # This is where we want to stop our loop
@@ -61,11 +100,12 @@ def select(function_code):
     return True
 
 
+
 running = True
 while running:
 
     selection = user_input(
-        "Press C to add to list, R to Read from list and P to display list: ")
+        "Press C to add to list, R to Read from list, U to update an item, D to remove an item, P to display list, and Q to exit: ")
     running = select(selection)
 
 
@@ -85,5 +125,3 @@ def test():
     # View results
     list_all_items()
     # Continue until all code is run
-
-# test()
